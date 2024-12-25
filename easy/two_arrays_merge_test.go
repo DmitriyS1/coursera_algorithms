2b1af001b55eca106eb1ca6401b08c7346cc3022
Dmitrii Semenov <dmitriy.a.semenov@outlook.com>
2024-12-25 12:49:03 +0000

    EASY: Finished TwoArray merge problem

---
+++ easy/two_arrays_merge_test.go
@@ -0,0 +1,20 @@
+package easy
+
+import "testing"
+
+func Test1(t *testing.T) {
+	m := 4
+	n := 3
+	nums1 := []int{1, 3, 8, 12, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+
+	Merge(nums1, m, nums2, n)
+
+	result := []int{1, 2, 3, 5, 6, 8, 12}
+	for i := 0; i < 7; i++ {
+		if result[i] != nums1[i] {
+			t.Errorf("Wrong answer, has to be %v, but got %v", result, nums1)
+			break
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module dmitriys1/courcera_algorithms
 
-go 1.21.1
+go 1.22.2
