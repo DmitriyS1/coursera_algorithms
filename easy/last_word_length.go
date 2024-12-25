b12f4a410f2b13cd40d9f1313aa85640bc213857
Dmitrii Semenov <dmitriy.a.semenov@outlook.com>
2024-12-25 13:16:42 +0000

    EASY: Solved length of the last word problem

---
+++ easy/last_word_length.go
@@ -0,0 +1,23 @@
+package easy
+
+import "unicode"
+
+func LengthOfLastWord(s string) int {
+	length := 0
+	lastSymbolIsSpace := false
+	for _, r := range s {
+		isLetter := unicode.IsLetter(r)
+		if isLetter && lastSymbolIsSpace {
+			length = 0
+			lastSymbolIsSpace = false
+		}
+
+		if isLetter {
+			length++
+		} else {
+			lastSymbolIsSpace = true
+		}
+	}
+
+	return length
+}
